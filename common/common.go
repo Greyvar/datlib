6621157cface8db01f850d99bb24a30806fc835f
jamesread <contact@jread.com>
2025-01-17 22:32:09 +0000

    datlib return dat dir as absolute

--- common/common.go
+++ common/common.go
@@ -6,8 +6,11 @@ import (
 
 func DatDir() string {
 	if _, err := os.Stat("dat"); err == nil {
-		return "dat"
+		// Return as a absolute path
+		absPath, _ := os.Getwd()
+		return absPath + "/dat"
 	}
+
 
 	return os.Getenv("GREYVAR_DAT_DIR")
 }
